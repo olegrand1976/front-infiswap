e27d649be2bd36869266d19d2d6b3dbfd22bf738
Lala <lala@mg-consulting-it-act.com>
2025-02-10 02:07:59 +0300

    feature: add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,6 +22,7 @@ export default defineNuxtConfig({
                 },
             ],
             link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
                 {
                     rel: 'stylesheet',
                     href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
