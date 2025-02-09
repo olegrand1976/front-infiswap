088f9c957614752cfe88d52c6f3534a561f62149
Lala <lala@mg-consulting-it-act.com>
2025-02-09 23:57:44 +0300

    chore: add spinner if inProgress - button component ui

--- components/ui/button/Button.vue
+++ components/ui/button/Button.vue
@@ -10,6 +10,7 @@ interface Props extends PrimitiveProps {
     size?: ButtonVariants['size'];
     class?: HTMLAttributes['class'];
     href?: string;
+    inProgress?: boolean;
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -23,7 +24,29 @@ const props = withDefaults(defineProps<Props>(), {
         :to="href"
         :class="cn(buttonVariants({ variant, size }), props.class)"
         v-bind="href ? {} : { type: 'button' }"
+        :disabled="inProgress"
     >
         <slot />
+        <svg
+            v-if="inProgress"
+            class="animate-spin h-5 w-5 text-white absolute right-3"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+        >
+            <circle
+                class="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                stroke-width="4"
+            />
+            <path
+                class="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8v4l3.5-3.5L12 4V0a12 12 0 00-8.49 20.49L4 12z"
+            />
+        </svg>
     </Primitive>
 </template>
