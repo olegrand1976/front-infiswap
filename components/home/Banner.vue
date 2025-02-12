9dae311bdb8abe6cda3bf035b21d5c9225975088
jerry <jerryandriam@gmail.com>
2025-02-12 11:28:40 +0300

    chore: adjust component banner

--- components/home/Banner.vue
+++ components/home/Banner.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="hidden sm:flex space-x-8 container mt-8">
-        <div class="lg:w-[60%] sm:w-[50%] bg-white shadow p-8 rounded-2xl flex lg:flex-row sm:flex-col lg:justify-start sm:justify-center sm:items-center sm:mx-auto space-x-6">
+        <div class="w-full bg-white shadow p-8 rounded-2xl flex lg:flex-row sm:flex-col lg:justify-start sm:justify-center sm:items-center sm:mx-auto space-x-6">
             <div class="xl:w-[40%] lg:w-[30%]">
                 <LayoutsLogo class="lg:w-auto sm:w-36"/>
             </div>
-            <div class="xl:w-[60%] lg:w-[70%] sm:mx-auto lg:mx-0 sm:flex sm:flex-col sm:justify-center lg:block ">
+            <div class="xl:w-[60%] lg:w-[70%] sm:mx-auto lg:mx-0 sm:flex sm:flex-col sm:justify-center items-center lg:block ">
                 <p class="xl:text-base md:text-sm sm:text-xs sm:text-center lg:text-start">
                     Votre plateforme dédiée aux infirmiers libéraux, facilitant les remplacements, la
                     gestion des tournées et le suivi des formations. Elle optimise la planification, réduit
@@ -21,24 +21,28 @@
             </div>
         </div>
 
-        <div class="lg:w-[40%] sm:w-[50%] relative">
+        <div class="w-full relative">
             <NuxtImg 
                 src="/container_register.png"
+                class="w-full"  
             />
-            <div class="absolute mx-2 xl:bottom-6 lg:bottom-3 md:bottom-28 sm:bottom-32 xl:text-base lg:text-sm sm:text-xs text-white left-6">
+            <div class="absolute ml-2 mr-16 2xl:bottom-[15%] xl:bottom-[12%] bottom-[5%] lg:text-sm sm:text-xs text-white left-6">
                 <h2 class="font-semibold">
-                    Lorem ipsum
+                    Inscrivez-vous dès maintenant
                 </h2>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Ad molestias doloremque maiores reprehenderit excepturi voluptates! 
+                <p class="mt-2">
+                    Profitez d'une remise incroyable de 50 % sur votre abonnement dès aujourd'hui. 
+                    Accédez à toutes nos fonctionnalités premium et bénéficiez d’un service de qualité
+                    à moitié prix ! Cette offre est limitée, alors n’attendez plus pour vous inscrire
+                    et démarrer votre expérience à prix réduit. Inscrivez-vous maintenant et 
+                    économisez dès votre première année ! 
                 </p>
                 <Button
                     variant="secondary"
                     href="/register"
-                    class="lg:mt-6 text-primary w-36 lg:text-sm md:text-xs md:mt-3"
+                    class="lg:mt-3 text-primary w-36 lg:text-sm md:text-xs md:mt-3"
                 >
-                    <span class="sm:text-xs md:text-sm lg:text-base">S'inscrire</span>
+                    <span class="sm:text-xs md:text-sm">S'inscrire</span>
                 </Button>
             </div>    
         </div>
