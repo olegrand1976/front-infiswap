f80fc34adefd605b87c89419aa593efb2f9a4148
Lala <lala@mg-consulting-it-act.com>
2025-02-12 09:33:35 +0300

    merge from jerry

--- components/home/Banner.vue
+++ components/home/Banner.vue
@@ -21,22 +21,27 @@
             </div>
         </div>
 
-        <!-- <div class="relative mt-32 sm:mt-auto sm:absolute flex flex-col justify-center sm:justify-start md:space-y-8 sm:space-y-4 space-y-10 xl:left-24 xl:top-20 xl:right-auto lg:right-[2rem] lg:top-16 lg:ms-auto md:top-32 md:mr-[40vw] sm:mr-[40%] sm:top-20">
-            <div>
-                <LayoutsLogo class="xl:w-[42rem] lg:w-[28rem] md:w-80 sm:w-64 w-[14.5rem] mx-auto md:mx-auto" />
-            </div>
-            <p class="xl:w-[28rem] lg:w-[28rem] md:w-[24rem] xl:text-base lg:text-sm sm:w-[19rem] text-xs text-center mx-8 sm:mx-auto" /><p>Infiswap, la solution dédiée aux infirmiers libéraux pour gérer efficacement les remplacements.</p>
-            <p class="m-4">
-                Trouvez rapidement un remplaçant fiable, simplifiez la planification et assurez la continuité des soins.
-            </p>
-            <Button
-                variant="outline"
-                class="rounded-full lg:text-sm text-xs mx-auto"
-                href="/"
-            >
-                Inscrivez-vous
-            </Button>
-        </div> -->
+        <div class="lg:w-[40%] sm:w-[50%] relative">
+            <NuxtImg 
+                src="/container_register.png"
+            />
+            <div class="absolute mx-2 xl:bottom-6 lg:bottom-3 md:bottom-28 sm:bottom-32 xl:text-base lg:text-sm sm:text-xs text-white left-6">
+                <h2 class="font-semibold">
+                    Lorem ipsum
+                </h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Ad molestias doloremque maiores reprehenderit excepturi voluptates! 
+                </p>
+                <Button
+                    variant="secondary"
+                    href="/register"
+                    class="lg:mt-6 text-primary w-36 lg:text-sm md:text-xs md:mt-3"
+                >
+                    <span class="sm:text-xs md:text-sm lg:text-base">S'inscrire</span>
+                </Button>
+            </div>    
+        </div>
     </div>
 
     <div class="sm:hidden flex flex-col justify-center items-center mt-40 mx-12 mb-24 space-y-10">
