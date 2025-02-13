ca27b60db9c642634148254956c4fa74333eba92
Lala <lala@mg-consulting-it-act.com>
2025-02-13 13:36:04 +0300

    feat:add message section in banner

--- components/home/Banner.vue
+++ components/home/Banner.vue
@@ -1,23 +1,24 @@
 <template>
     <div class="hidden sm:flex space-x-8 container mt-8">
-        <div class="w-full bg-white shadow p-8 rounded-2xl flex lg:flex-row sm:flex-col lg:justify-start sm:justify-center sm:items-center sm:mx-auto space-x-6">
-            <div class="xl:w-[40%] lg:w-[30%]">
-                <LayoutsLogo class="lg:w-auto sm:w-36" />
+        <div class="w-full bg-pink-200 shadow p-8 rounded-2xl flex lg:flex-row sm:flex-col lg:justify-start sm:justify-center sm:items-center sm:mx-auto space-x-6">
+            <div class="xl:w-[40%] lg:w-[30%] grid gap-2 place-content-center h-full w-1/2">
+                <!-- <LayoutsLogo class="lg:w-auto sm:w-36" /> -->
+                 <NuxtImg src="/home/mail-image.png"/>
             </div>
             <div class="xl:w-[60%] lg:w-[70%] sm:mx-auto lg:mx-0 sm:flex sm:flex-col sm:justify-center items-center lg:block ">
-                <p class="xl:text-base md:text-sm sm:text-xs sm:text-center lg:text-start">
-                    Votre plateforme dédiée aux infirmiers libéraux, facilitant les remplacements, la
-                    gestion des tournées et le suivi des formations. Elle optimise la planification, réduit
-                    le temps administratif et améliore la coordination, offrant ainsi plus d'efficacité, de
-                    flexibilité et une meilleure qualité de soins.
-                </p>
-                <Button
+                <div class="xl:text-base md:text-sm mt-4 sm:text-xs sm:text-center lg:text-start">
+                    <p class="font-semibold mb-4">Chers utilisateurs,</p>
+                    <p>Nous sommes ravis de vous annoncer que notre plateforme ouvrira ses portes à partir de la deuxième moitié du mois de mars. Nous travaillons d'arrache-pied pour vous offrir une expérience optimale et innovante. Restez connectés pour découvrir nos prochaines annonces et mises à jour.</p>
+
+                    <p>Merci de votre confiance et à très bientôt !</p>
+                </div>
+                <!-- <Button
                     variant="outline"
                     class="rounded-full mt-6"
                     href="/contact"
                 >
                     <span class="sm:text-xs md:text-sm lg:text-base">Contactez-nous</span>
-                </Button>
+                </Button> -->
             </div>
         </div>
 
@@ -50,14 +51,20 @@
 
     <div class="sm:hidden flex flex-col justify-center items-center mt-40 mx-12 mb-24 space-y-10">
         <div>
-            <LayoutsLogo class="w-64" />
+            <!-- <LayoutsLogo class="w-64" /> -->
+            <NuxtImg src="/home/mail-image.png"/>
         </div>
         <div>
             <p class="text-xs text-center">
-                Votre plateforme dédiée aux infirmiers libéraux, facilitant les remplacements, la
+                <!-- Votre plateforme dédiée aux infirmiers libéraux, facilitant les remplacements, la
                 gestion des tournées et le suivi des formations. Elle optimise la planification, réduit
                 le temps administratif et améliore la coordination, offrant ainsi plus d'efficacité, de
-                flexibilité et une meilleure qualité de soins.
+                flexibilité et une meilleure qualité de soins. -->
+                <p class="font-semibold mb-4">Chers utilisateurs,</p>
+                <p>Nous sommes ravis de vous annoncer que notre plateforme ouvrira ses portes à partir de la deuxième moitié du mois de mars. Nous travaillons d'arrache-pied pour vous offrir une expérience optimale et innovante. Restez connectés pour découvrir nos prochaines annonces et mises à jour.</p>
+
+                <p>Merci de votre confiance et à très bientôt !</p>
+
             </p>
         </div>
     </div>
