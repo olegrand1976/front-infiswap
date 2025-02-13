63255ae86fb49d9a636e0ff40d4e964f0412325b
olivier <o.legrand1976@gmail.com>
2025-02-13 15:30:32 +0100

    add baseurl in nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,6 +22,7 @@ export default defineNuxtConfig({
     devtools: { enabled: false },
 
     app: {
+        baseURL: '/',
         head: {
             titleTemplate: '%s | InfiSwap',
             meta: [
