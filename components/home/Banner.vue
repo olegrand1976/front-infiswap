91f87d56c7eef576f9a24ac8eb0d963ce6554234
Lala <lala@mg-consulting-it-act.com>
2025-02-13 19:28:24 +0300

    fix: banner component class

--- components/home/Banner.vue
+++ components/home/Banner.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="container font-light grid grid-cols-5 gap-4 justify-between mt-10">
+    <div class="container font-light grid grid-cols-5 gap-8 justify-between mt-10">
         <div class="col-span-5 lg:col-span-3 grid grid-cols-2 items-center shadow-lg rounded-lg overflow-hidden">
-            <div class="relative col-span-3 lg:col-span-2 h-full w-full max-h-72 bg-red-200">
+            <div class="relative h-60 col-span-2 lg:col-span-1 lg:h-full w-full bg-red-200">
                  <NuxtImg
                     src="/home/message_home_background.png"
                     class="w-full h-full"
                 />
                  <NuxtImg
                     src="/home/mail-image.png"
-                    class="absolute w-36 top-1/2 bottom-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                    class="absolute w-32 lg:w-36 top-1/2 bottom-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
                  />
             </div>
-            <div class="p-4 col-span-2 lg:col-span-4 text-start lg:text-start">
+            <div class="p-4 col-span-2 lg:col-span-1 text-start lg:text-start">
                 <p class="font-bold mb-4">Chers utilisateurs,</p>
                 <p class="mb-4">Nous sommes ravis de vous annoncer que notre plateforme ouvrira ses portes à partir de la deuxième moitié du mois de mars. Nous travaillons d'arrache-pied pour vous offrir une expérience optimale et innovante. Restez connectés pour découvrir nos prochaines annonces et mises à jour.</p>
 
