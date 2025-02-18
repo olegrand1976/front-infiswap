c30c174c4f34102dd1293a78ee03f6b40a53aead
jerry <jerryandriam@gmail.com>
2025-02-18 13:26:53 +0300

    feat: implements my planning page layout

---
+++ pages/my-planning.vue
@@ -0,0 +1,22 @@
+<template>
+    <NuxtLayout name="guest">
+        <template #title>
+            Mon <strong>planning</strong>
+        </template>
+        <template #logo>
+            <LayoutsLogo class="w-96 md:w-80" />
+        </template>
+    </NuxtLayout>
+
+    <!-- ETO LAY CONTENU NO ATAO
+    -->
+
+    <!-- ETO LAY CONTENU NO ATAO
+     -->
+</template>
+
+<script lang="ts" setup>
+useHead({
+    title: 'Mon planning',
+});
+</script>
