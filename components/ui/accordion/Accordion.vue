ece0724c4dcb3a65abb2de1496ff4cd5e0a63e48
jerry <jerryandriam@gmail.com>
2025-02-19 12:22:53 +0300

    fix: lint page

--- components/ui/accordion/Accordion.vue
+++ components/ui/accordion/Accordion.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
-import type { AccordionRootEmits, AccordionRootProps } from 'radix-vue'
+import type { AccordionRootEmits, AccordionRootProps } from 'radix-vue';
 import {
-  AccordionRoot,
+    AccordionRoot,
 
-  useForwardPropsEmits,
-} from 'radix-vue'
+    useForwardPropsEmits,
+} from 'radix-vue';
 
-const props = defineProps<AccordionRootProps>()
-const emits = defineEmits<AccordionRootEmits>()
+const props = defineProps<AccordionRootProps>();
+const emits = defineEmits<AccordionRootEmits>();
 
-const forwarded = useForwardPropsEmits(props, emits)
+const forwarded = useForwardPropsEmits(props, emits);
 </script>
 
 <template>
-  <AccordionRoot v-bind="forwarded">
-    <slot />
-  </AccordionRoot>
+    <AccordionRoot v-bind="forwarded">
+        <slot />
+    </AccordionRoot>
 </template>
--- components/ui/accordion/AccordionItem.vue
+++ components/ui/accordion/AccordionItem.vue
@@ -1,26 +1,26 @@
 <script setup lang="ts">
-import type { AccordionItemProps } from 'radix-vue'
-import type { HTMLAttributes } from 'vue'
-import { cn } from '@/lib/utils'
-import { AccordionItem, useForwardProps } from 'radix-vue'
-import { computed } from 'vue'
+import type { AccordionItemProps } from 'radix-vue';
+import type { HTMLAttributes } from 'vue';
+import { AccordionItem, useForwardProps } from 'radix-vue';
+import { computed } from 'vue';
+import { cn } from '@/lib/utils';
 
-const props = defineProps<AccordionItemProps & { class?: HTMLAttributes['class'] }>()
+const props = defineProps<AccordionItemProps & { class?: HTMLAttributes['class'] }>();
 
 const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props
+    const { class: _, ...delegated } = props;
 
-  return delegated
-})
+    return delegated;
+});
 
-const forwardedProps = useForwardProps(delegatedProps)
+const forwardedProps = useForwardProps(delegatedProps);
 </script>
 
 <template>
-  <AccordionItem
-    v-bind="forwardedProps"
-    :class="cn('border-b', props.class)"
-  >
-    <slot />
-  </AccordionItem>
+    <AccordionItem
+        v-bind="forwardedProps"
+        :class="cn('border-b', props.class)"
+    >
+        <slot />
+    </AccordionItem>
 </template>
