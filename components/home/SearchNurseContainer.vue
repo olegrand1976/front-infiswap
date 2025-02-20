33505072c03f85f179c1bcf9dfd86b4e040c3f17
Lala <lala@mg-consulting-it-act.com>
2025-02-21 02:13:58 +0300

    refactor

--- components/home/SearchNurseContainer.vue
+++ components/home/SearchNurseContainer.vue
@@ -3,27 +3,24 @@
         <NuxtImg
             src="/home/search_nurse_mobile.png"
             alt="Bannière rechercher une infirmière"
-            class="lg:hidden w-full object-cover md:h-96 lg:h-auto"
+            class="w-full object-cover md:h-96 lg:hidden"
         />
 
         <NuxtImg
             src="/home/search_nurse_image.png"
             alt="Bannière rechercher une infirmière"
-            class="hidden lg:inline-block -ml-2 w-11/12 object-cover"
+            class="hidden lg:inline-block -ml-20 w-full object-cover"
         />
 
-        <div class="absolute lg:ml-4 font-secondary w-full italic text-white transform top-1/2 -translate-y-1/5 lg:-translate-y-1/2">
-            <div class="w-full container grid place-content-center text-center lg:text-left lg:ml-20 2xl:ml-0">
-                <p class="text-3xl sm:text-5xl lg:text-6xl font-extralight">
-                    Je cherche
-                </p>
-                <p class="text-4xl lg:text-8xl 3xl:text-12xl font-bold mt-2">
-                    Un(e) infirmier(e)
-                </p>
+        <div class="absolute top-1/2 w-full transform -translate-y-1/5 lg:-translate-y-1/2 text-white font-secondary italic">
+            <div class="container mx-auto grid place-content-center text-center lg:text-left">
+                <p class="text-3xl sm:text-5xl lg:text-6xl font-extralight">Je cherche</p>
+                <p class="mt-2 text-4xl lg:text-7xl 3xl:text-12xl font-bold">Un(e) infirmier(e)</p>
+
                 <Button
                     variant="secondary"
                     href="/search-nurse"
-                    class="font-bold mt-4 mx-auto lg:ml-0 lg:mt-6 text-primary w-full max-w-32"
+                    class="mt-4 lg:mt-6 font-bold text-primary w-full max-w-32 mx-auto lg:mx-0"
                 >
                     Consulter
                 </Button>
