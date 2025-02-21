322f413878084efd41573101cf2dd258f9c6e6f0
mialivola <mialivolarandrianaivo@gmail.com>
2025-02-21 16:31:41 +0300

    Suppression du logo dans la page a propos

--- layouts/guest.vue
+++ layouts/guest.vue
@@ -2,7 +2,7 @@
     <div class="bg-white flex flex-col">
         <header>
             <LayoutsHeader />
-            <div class="mt-32 sm:mt-0 sm:bg-tertiary/30 sm:h-80 flex justify-center items-center relative">
+            <!-- <div class="mt-32 sm:mt-0 sm:bg-tertiary/30 sm:h-80 flex justify-center items-center relative">
                 <h1 class="text-primary text-3xl lg:text-4xl text-center">
                     <slot
                         name="title"
@@ -11,7 +11,7 @@
                 <div class="hidden sm:block absolute -bottom-12 inset-x-auto">
                     <slot name="logo" />
                 </div>
-            </div>
+            </div> -->
         </header>
         <main class="flex-1 mb-24">
             <slot />
