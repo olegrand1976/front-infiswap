096b9427f08c6eb4f5bc6ad0eed0948bc28f17ca
mialivola <mialivolarandrianaivo@gmail.com>
2025-02-21 16:21:27 +0300

    Modification avec container page a propos

--- pages/about.vue
+++ pages/about.vue
@@ -1,62 +1,63 @@
 <template>
     <NuxtLayout name="guest">
-        <template #title>
+        <!-- <template #title>
             <LayoutsLogo class="w-96 flex justify-center items-center mx-auto" />
-        </template>
+        </template> -->
 
         <!-- <div class="mt-48">
             <LayoutsGridCard />
         </div> -->
 
         <div class="relative">
-            <!-- Background uniquement sur desktop avec hauteur ajustée sur mobile -->
             <div class="absolute inset-0 bg-tertiary/30 h-[200px] lg:h-full w-full"></div>
-            <div class="relative my-8 lg:my-16 z-20 grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-44 items-center p-8 lg:p-20">
-                <div class="lg:hidden text-center mb-8">
-                    <h2 class="text-primary text-4xl font-semibold">À propos</h2>
+            <div class="container">
+                <div class="relative my-8 lg:my-16 z-20 grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-44 items-center py-8 lg:py-20">
+                    <div class="lg:hidden text-center mb-8">
+                        <h2 class="text-primary text-4xl font-semibold">À propos</h2>
+                    </div>
+                    <div class="space-y-6 order-2 lg:order-1 relative z-30">
+                        <h3 class="text-primary md:text-3xl text-2xl text-center md:text-left">
+                            Infiswap...
+                        </h3>
+                        <p>
+                            est une plateforme innovante conçue pour optimiser
+                            la gestion des infirmiers libéraux. Elle simplifie les
+                            remplacements, la planification des tournées et le suivi
+                            des formations. Grâce à des outils intelligents et automatisés,
+                            Infiswap améliore la coordination, réduit le temps administratif
+                            et facilite l'organisation quotidienne, offrant ainsi une meilleure
+                            qualité de soins et un gain de temps précieux.
+                        </p>
+                        <p>
+                            la gestion des tournées est optimisée avec une planification automatisée,
+                            prenant en compte les contraintes horaires, les types de soins et la
+                            réduction des déplacements inutiles.
+                        </p>
+                        <p>
+                            Infiswap permet également un suivi complet des formations professionnelles,
+                            assurant la conformité aux obligations légales avec des rappels et une gestion
+                            simplifiée des inscriptions. <br>
+                            Le suivi des patients est amélioré par un historique détaillé des soins,
+                            facilitant la transmission entre soignants.
+                        </p>
+                        <Button href="/register">
+                            S'inscrire
+                        </Button>
+                    </div>
+                    <div class="order-1 lg:order-2 flex justify-center lg:justify-end">
+                        <NuxtImg
+                            src="/home/nurse_with_old_woman2.png"
+                            alt="Infirmière avec une vieille femme"
+                            class="w-full max-w-2xs lg:max-w-none z-20"
+                        />
+                    </div>
                 </div>
-                <div class="space-y-6 order-2 lg:order-1 relative z-30">
-                    <h3 class="text-primary md:text-3xl text-2xl text-center md:text-left">
-                        Infiswap...
-                    </h3>
-                    <p>
-                        est une plateforme innovante conçue pour optimiser
-                        la gestion des infirmiers libéraux. Elle simplifie les
-                        remplacements, la planification des tournées et le suivi
-                        des formations. Grâce à des outils intelligents et automatisés,
-                        Infiswap améliore la coordination, réduit le temps administratif
-                        et facilite l'organisation quotidienne, offrant ainsi une meilleure
-                        qualité de soins et un gain de temps précieux.
-                    </p>
-                    <p>
-                        la gestion des tournées est optimisée avec une planification automatisée,
-                        prenant en compte les contraintes horaires, les types de soins et la
-                        réduction des déplacements inutiles.
-                    </p>
-                    <p>
-                        Infiswap permet également un suivi complet des formations professionnelles,
-                        assurant la conformité aux obligations légales avec des rappels et une gestion
-                        simplifiée des inscriptions. <br>
-                        Le suivi des patients est amélioré par un historique détaillé des soins,
-                        facilitant la transmission entre soignants.
-                    </p>
-                    <Button href="/register">
-                        S'inscrire
-                    </Button>
-                </div>
-                <div class="order-1 lg:order-2 flex justify-center lg:justify-end">
-                    <NuxtImg
-                        src="/home/nurse_with_old_woman2.png"
-                        alt="Infirmière avec une vieille femme"
-                        class="w-full max-w-xs lg:max-w-none z-20"
-                    />
-                </div>
-                <div class="absolute bottom-0 right-0 hidden lg:block">
-                    <NuxtImg src="/Vector_2.png" class="w-64 lg:w-[32rem] -z-10 relative"/>
-                </div>
-                <div class="absolute bottom-[20%] left-2 hidden lg:block">
-                    <NuxtImg src="/icons/Unin.png" class="w-8 lg:w-12 -z-10 relative"/>
-                </div>
+            </div>  
+            <div class="absolute bottom-0 right-0 hidden lg:block z-0">
+                <NuxtImg src="/Vector_2.png" class="w-64 lg:w-[32rem] relative"/>
+            </div>
+            <div class="absolute bottom-[20%] left-2 hidden lg:block z-0">
+                <NuxtImg src="/icons/Unin.png" class="w-8 lg:w-12 relative"/>
             </div>
         </div>
 
@@ -68,46 +69,48 @@
             <HomeSubscribeBanner />
         </div>
 
-        <div class="relative my-8 lg:my-16 z-20 grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-44 items-center p-8 lg:p-20">
-            <div>
-                <NuxtImg
-                    src="/home/black_nurse.png"
-                    alt="Jeune infirmière noire"
-                    class="w-full"
-                />
-            </div>
+        <div class="container">
+            <div class="relative my-8 lg:my-16 z-20 grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-44 items-center py-8 lg:py-20">
+                <div>
+                    <NuxtImg
+                        src="/home/black_nurse.png"
+                        alt="Jeune infirmière noire"
+                        class="w-full"
+                    />
+                </div>
 
-            <div class="space-y-6">
-                <h3 class="text-xl md:text-3xl text-primary text-center md:text-left">
-                    Enfin, <strong>la plateforme...</strong>
-                </h3>
-                <p>
-                    Intègre des outils de gestion administrative et financière,
-                    notamment le suivi des véhicules, la facturation et la
-                    centralisation des informations essentielles.
-                </p>
-                <p>
-                    Accessible sur mobile et ordinateur, Infiswap apporte un gain de
-                    temps significatif, améliore la coordination entre professionnels et
-                    contribue à une meilleure qualité de soins. Une solution moderne et
-                    indispensable pour un exercice serein et efficace du métier d'infirmier. <br>
-                    Infiswap est une plateforme dédiée aux infirmiers libéraux, facilitant les
-                    remplacements, la gestion des tournées et le suivi des formations. Elle
-                    optimise la planification, réduit le temps administratif et améliore la
-                    coordination, offrant ainsi plus d'efficacité, de flexibilité et une
-                    meilleure qualité de soins.
-                </p>
-                <p>
-                    Avec infiswap, trouvez et gérez vos remplacements facilement. Plus de visibilité
-                    pour les annonces, une recherche optimisée par localisation et une mise en relation
-                    rapide entre infirmiers. Gagnez du temps, développez votre réseau et exercez en toute
-                    sérénité!
-                </p>
-                <Button
-                    href="/register"
-                >
-                    S'inscrire
-                </Button>
+                <div class="space-y-6">
+                    <h3 class="text-xl md:text-3xl text-primary text-center md:text-left">
+                        Enfin, <strong>la plateforme...</strong>
+                    </h3>
+                    <p>
+                        Intègre des outils de gestion administrative et financière,
+                        notamment le suivi des véhicules, la facturation et la
+                        centralisation des informations essentielles.
+                    </p>
+                    <p>
+                        Accessible sur mobile et ordinateur, Infiswap apporte un gain de
+                        temps significatif, améliore la coordination entre professionnels et
+                        contribue à une meilleure qualité de soins. Une solution moderne et
+                        indispensable pour un exercice serein et efficace du métier d'infirmier. <br>
+                        Infiswap est une plateforme dédiée aux infirmiers libéraux, facilitant les
+                        remplacements, la gestion des tournées et le suivi des formations. Elle
+                        optimise la planification, réduit le temps administratif et améliore la
+                        coordination, offrant ainsi plus d'efficacité, de flexibilité et une
+                        meilleure qualité de soins.
+                    </p>
+                    <p>
+                        Avec infiswap, trouvez et gérez vos remplacements facilement. Plus de visibilité
+                        pour les annonces, une recherche optimisée par localisation et une mise en relation
+                        rapide entre infirmiers. Gagnez du temps, développez votre réseau et exercez en toute
+                        sérénité!
+                    </p>
+                    <Button
+                        href="/register"
+                    >
+                        S'inscrire
+                    </Button>
+                </div>
             </div>
         </div>
 
