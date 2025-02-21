8db04fc0e4115a11160b0c7e456111388938ed82
mialivola <mialivolarandrianaivo@gmail.com>
2025-02-21 10:10:55 +0300

    Chore frontend about Mialivola

--- pages/about.vue
+++ pages/about.vue
@@ -4,63 +4,59 @@
             <LayoutsLogo class="w-96 flex justify-center items-center mx-auto" />
         </template>
 
-        <div class="mt-48">
+        <!-- <div class="mt-48">
             <LayoutsGridCard />
-        </div>
+        </div> -->
 
-        <div class="my-16 z-20 relative grid grid-cols-2 gap-44 items-center bg-tertiary/30 p-20">
-            <div class="space-y-6">
-                <h3 class="text-primary text-3xl">
-                    Infiswap...
-                </h3>
-                <p>
-                    est une plateforme innovante conçue pour optimiser
-                    la gestion des infirmiers libéraux. Elle simplifie les
-                    remplacements, la planification des tournées et le suivi
-                    des formations. Grâce à des outils intelligents et automatisés,
-                    Infiswap améliore la coordination, réduit le temps administratif
-                    et facilite l'organisation quotidienne, offrant ainsi une meilleure
-                    qualité de soins et un gain de temps précieux.
-                </p>
-                <p>
-                    la gestion des tournées est optimisée avec une planification automatisée,
-                    prenant en compte les contraintes horaires, les types de soins et la
-                    réduction des déplacements inutiles.
-                </p>
-                <p>
-                    Infiswap permet également un suivi complet des formations professionnelles,
-                    assurant la conformité aux obligations légales avec des rappels et une gestion
-                    simplifiée des inscriptions. <br>
-                    Le suivi des patients est amélioré par un historique détaillé des soins,
-                    facilitant la transmission entre soignants.
-                </p>
-                <Button
-                    href="/register"
-                >
-                    S'inscrire
-                </Button>
-            </div>
-
-            <div>
-                <NuxtImg
-                    src="/home/nurse_with_old_woman2.png"
-                    alt="Infirmière avec une vieille femme"
-                    class="w-full z-20"
-                />
-            </div>
-
-            <div class="absolute bottom-0 right-0">
-                <NuxtImg
-                    src="/Vector_2.png"
-                    class="w-[32rem] -z-10 relative"
-                />
-            </div>
-
-            <div class="absolute bottom-[20%] left-2">
-                <NuxtImg
-                    src="/icons/Unin.png"
-                    class="w-12 -z-10 relative"
-                />
+        <div class="relative">
+            <!-- Background uniquement sur desktop avec hauteur ajustée sur mobile -->
+            <div class="absolute inset-0 bg-tertiary/30 h-[200px] lg:h-full w-full"></div>
+            <div class="relative my-8 lg:my-16 z-20 grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-44 items-center p-8 lg:p-20">
+                <div class="lg:hidden text-center mb-8">
+                    <h2 class="text-primary text-4xl font-semibold">À propos</h2>
+                </div>
+                <div class="space-y-6 order-2 lg:order-1 relative z-30">
+                    <h3 class="text-primary md:text-3xl text-2xl text-center md:text-left">
+                        Infiswap...
+                    </h3>
+                    <p>
+                        est une plateforme innovante conçue pour optimiser
+                        la gestion des infirmiers libéraux. Elle simplifie les
+                        remplacements, la planification des tournées et le suivi
+                        des formations. Grâce à des outils intelligents et automatisés,
+                        Infiswap améliore la coordination, réduit le temps administratif
+                        et facilite l'organisation quotidienne, offrant ainsi une meilleure
+                        qualité de soins et un gain de temps précieux.
+                    </p>
+                    <p>
+                        la gestion des tournées est optimisée avec une planification automatisée,
+                        prenant en compte les contraintes horaires, les types de soins et la
+                        réduction des déplacements inutiles.
+                    </p>
+                    <p>
+                        Infiswap permet également un suivi complet des formations professionnelles,
+                        assurant la conformité aux obligations légales avec des rappels et une gestion
+                        simplifiée des inscriptions. <br>
+                        Le suivi des patients est amélioré par un historique détaillé des soins,
+                        facilitant la transmission entre soignants.
+                    </p>
+                    <Button href="/register">
+                        S'inscrire
+                    </Button>
+                </div>
+                <div class="order-1 lg:order-2 flex justify-center lg:justify-end">
+                    <NuxtImg
+                        src="/home/nurse_with_old_woman2.png"
+                        alt="Infirmière avec une vieille femme"
+                        class="w-full max-w-xs lg:max-w-none z-20"
+                    />
+                </div>
+                <div class="absolute bottom-0 right-0 hidden lg:block">
+                    <NuxtImg src="/Vector_2.png" class="w-64 lg:w-[32rem] -z-10 relative"/>
+                </div>
+                <div class="absolute bottom-[20%] left-2 hidden lg:block">
+                    <NuxtImg src="/icons/Unin.png" class="w-8 lg:w-12 -z-10 relative"/>
+                </div>
             </div>
         </div>
 
@@ -72,7 +68,7 @@
             <HomeSubscribeBanner />
         </div>
 
-        <div class="my-36 container grid grid-cols-2 items-center gap-20">
+        <div class="relative my-8 lg:my-16 z-20 grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-44 items-center p-8 lg:p-20">
             <div>
                 <NuxtImg
                     src="/home/black_nurse.png"
@@ -82,9 +78,8 @@
             </div>
 
             <div class="space-y-6">
-                <h3 class="text-3xl text-primary">
-                    Enfin,
-                    <strong>la plateforme...</strong>
+                <h3 class="text-xl md:text-3xl text-primary text-center md:text-left">
+                    Enfin, <strong>la plateforme...</strong>
                 </h3>
                 <p>
                     Intègre des outils de gestion administrative et financière,
@@ -121,21 +116,33 @@
                 v-for="(card, index) in cards"
                 :key="index"
             >
-                <Card class="border border-gray-300 hover:bg-transparent hover:scale-100 cursor-default mb-6">
+                <Card class="border border-gray-300 hover:bg-transparent hover:scale-100 cursor-default mb-6 -ml-4 -mr-4">
                     <CardHeader class="p-4 rounded-lg bg-primary text-white">
-                        <CardTitle>
+                        <CardTitle class="text-xs sm:text-lg">
                             {{ card.title }}
                         </CardTitle>
                     </CardHeader>
-
-                    <CardContent class="p-4 text-sm text-black">
-                        <p
-                            v-for="(item, key) in card.content"
-                            :key="key"
-                            class="m-0 p-0 leading-5"
-                        >
-                            {{ item }}
-                        </p>
+                    <CardContent class="p-4 text-sm text-black space-y-0">
+                        <div class="hidden sm:block sm:space-y-6">
+                            <p
+                                v-for="(item, key) in card.content"
+                                :key="key"
+                                class="m-0 p-0 leading-5 text-xs sm:text-sm"
+                            >
+                                {{ item }}
+                            </p>
+                        </div>
+                        <div class="sm:hidden pl-1">
+                            <ul class="list-disc pl-1 space-y-2">
+                                <li
+                                    v-for="(item, key) in card.content"
+                                    :key="key"
+                                    class="text-black text-xs sm:text-sm leading-5 list-item marker:text-[#d83b58]"
+                                >
+                                    {{ item }}
+                                </li>
+                            </ul>
+                        </div>
                     </CardContent>
                 </Card>
             </div>
