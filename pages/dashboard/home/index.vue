3508a165b10d21922a1d68509bc7ad7cc282c764
jerry <jerryandriam@gmail.com>
2025-03-07 12:18:24 +0300

    chore: implements tournee section

--- pages/dashboard/home/index.vue
+++ pages/dashboard/home/index.vue
@@ -79,38 +79,96 @@
             </div>
         </section>
 
-        <section class="grid grid-cols-[35%_63%] gap-4">
-            <div class="bg-gray-100 rounded p-4">
-                <h3 class="text-primary">
-                    Mes préférences
-                </h3>
+        <section class="grid grid-cols-[45%_53%] gap-4">
+            <div class="flex flex-col space-y-4">
+                <div class="bg-gray-100 rounded p-4">
+                    <h3 class="text-primary">
+                        Mes préférences
+                    </h3>
 
-                <Form class="mt-6 space-y-4">
-                    <div class="grid grid-cols-[40%_60%] border border-primary h-9 rounded-full">
-                        <div class="bg-primary flex items-center text-white ps-4 rounded-s-full">
-                            <label for="zipCode">
-                                Code postal
-                            </label>
+                    <Form class="mt-6 space-y-4">
+                        <div class="grid grid-cols-[40%_60%] border border-primary h-9 rounded-full">
+                            <div class="bg-primary flex items-center text-white ps-4 rounded-s-full">
+                                <label for="zipCode">
+                                    Code postal
+                                </label>
+                            </div>
+                            <Input
+                                v-model="formData.zipCode"
+                                type="text"
+                                class="w-full bg-transparent"
+                            />
                         </div>
-                        <Input
-                            v-model="formData.zipCode"
-                            type="text"
-                            class="w-full bg-transparent"
-                        />
-                    </div>
-                    <div class="grid grid-cols-[40%_60%] border border-primary h-9 rounded-full">
-                        <div class="bg-primary flex items-center text-white ps-4 rounded-s-full">
-                            <label for="city">
-                                Ville
-                            </label>
+                        <div class="grid grid-cols-[40%_60%] border border-primary h-9 rounded-full">
+                            <div class="bg-primary flex items-center text-white ps-4 rounded-s-full">
+                                <label for="city">
+                                    Ville
+                                </label>
+                            </div>
+                            <Input
+                                v-model="formData.city"
+                                type="text"
+                                class="w-full bg-transparent"
+                            />
                         </div>
-                        <Input
-                            v-model="formData.city"
-                            type="text"
-                            class="w-full bg-transparent"
-                        />
+                    </Form>
+                </div>
+
+                <div class="bg-gray-100 rounded p-4">
+                    <h3 class="text-primary">
+                        Tournée du jour
+                    </h3>
+
+                    <div class="my-3">
+                        <Table>
+                            <TableHeader class="w-full">
+                                <TableRow class="grid grid-cols-3 overflow-x-hidden gap-2 border border-none">
+                                    <TableHead class="bg-primary flex justify-center items-center rounded-lg text-white s">
+                                        Patient
+                                    </TableHead>
+                                    <TableHead class="bg-primary flex justify-center items-center rounded-lg text-white s">
+                                        Code postal
+                                    </TableHead>
+                                    <TableHead class="bg-primary flex justify-center items-center rounded-lg text-white s">
+                                        Ville
+                                    </TableHead>
+                                </TableRow>
+                            </TableHeader>
+
+                            <TableBody>
+                                <div
+                                    v-for="(tournee, index) in tournees"
+                                    :key="index"
+                                >
+                                    <TableRow class="cursor-pointer grid grid-cols-3 gap-2 border border-none overflow-x-hidden">
+                                        <TableCell class="bg-gray-100">
+                                            <div class="flex h-10 rounded mt-3 bg-gray-200 justify-center items-center">
+                                                <span class="truncate w-full px-2 text-center mx-auto">
+                                                    {{ tournee.patient }}
+                                                </span>
+                                            </div>
+                                        </TableCell>
+                                        <TableCell class="bg-gray-100">
+                                            <div class="flex h-10 rounded mt-3 bg-gray-200 justify-center items-center">
+                                                <span class="truncate w-full px-2 text-center mx-auto">
+                                                    {{ tournee.zip_code }}
+                                                </span>
+                                            </div>
+                                        </TableCell>
+                                        <TableCell class="bg-gray-100">
+                                            <div class="flex h-10 rounded mt-3 bg-gray-200 justify-center items-center">
+                                                <span class="truncate w-full px-2 text-center mx-auto">
+                                                    {{ tournee.city }}
+                                                </span>
+                                            </div>
+                                        </TableCell>
+                                    </TableRow>
+                                    <hr class="border-t border-gray-300">
+                                </div>
+                            </TableBody>
+                        </Table>
                     </div>
-                </Form>
+                </div>
             </div>
 
             <div class="bg-gray-100 rounded-b-lg">
@@ -186,12 +244,24 @@ const chartData = ref(
     ),
 );
 
+const tournees = [
+    {
+        patient: 'Jean Dupont',
+        zip_code: '12345',
+        city: 'Paris',
+    },
+    {
+        patient: 'Jean Dupont',
+        zip_code: '12345',
+        city: 'Paris',
+    },
+];
+
 useHead({
     title: 'Tableau de bord',
 });
 
 definePageMeta({
     layout: 'dashboard',
-    middleware: ['auth'],
 });
 </script>
