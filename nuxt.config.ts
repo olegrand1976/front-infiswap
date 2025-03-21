3fd8a6201ddf9c613d213baa80343b12b9c18c30
Lala <lala@mg-consulting-it-act.com>
2025-03-21 09:13:14 +0300

    reset nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,8 @@ export default defineNuxtConfig({
         process.env.NODE_ENV !== 'production' ? 'shadcn-nuxt' : null,
         '@nuxt/image',
     ].filter(Boolean),
-
-    plugins: [],
+    plugins: [
+    ],
 
     imports: {
         dirs: [
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
     },
 
     // Active les devtools uniquement en développement
-    devtools: { enabled: process.env.NODE_ENV === 'development' },
+    devtools: { enabled: true },
 
     app: {
         baseURL: '/',
@@ -55,6 +55,7 @@ export default defineNuxtConfig({
         },
     },
 
+    // Autres optimisations de build (minification, etc.)
     build: {
         optimization: {
             minimize: true,
@@ -76,9 +77,11 @@ export default defineNuxtConfig({
 
     nitro: {
         prerender: {
-            routes: ['/', '/about', '/contact'],
-            ignore: ['/_ipx/**'],
-            failOnError: false,
+            routes: [
+                '/',
+                '/about',
+                '/contact',
+            ],
         },
     },
 
@@ -92,7 +95,6 @@ export default defineNuxtConfig({
             minify: 'esbuild',
         },
     },
-
     gtm: {
         id: 'GTM-KFBFVVR3',
         queryParams: {
@@ -109,19 +111,10 @@ export default defineNuxtConfig({
         enableRouterSync: true,
         ignoredViews: ['dashboard'],
         trackOnNextTick: true,
-        // Conditionner les devtools à l'environnement de développement
-        devtools: process.env.NODE_ENV !== 'production',
+        devtools: true,
     },
-
-    // Configuration de Nuxt Image pour servir vos images depuis le dossier public
     image: {
-        provider: 'ipx',
         dir: 'assets/images',
-        domains: ['localhost', 'www.infiswap.be'],
-        format: ['webp', 'jpg', 'png', 'svg'],
-        ipx: {
-            baseURL: '/_ipx',
-        },
     },
 
     shadcn: {
