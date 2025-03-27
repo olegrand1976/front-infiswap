ff32d353523a05c601439fe0e4dbd243fd2cdd6e
Lala <lala@mg-consulting-it-act.com>
2025-03-27 07:49:15 +0300

    add subscribed middleware in replacement details

--- pages/dashboard/replacements/detail/[id]/index.vue
+++ pages/dashboard/replacements/detail/[id]/index.vue
@@ -294,7 +294,7 @@ useHead({
 
 definePageMeta({
     layout: 'dashboard',
-    middleware: ['auth', 'verified'],
+    middleware: ['auth', 'verified', 'subscribed'],
     ssr: false,
 });
 </script>
