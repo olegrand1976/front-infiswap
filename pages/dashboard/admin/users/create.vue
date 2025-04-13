f5e09834c3ee93b2b617d58610fc9d522a04c744
Lala <lala@mg-consulting-it-act.com>
2025-04-13 23:37:14 +0300

    chore: save work

--- pages/dashboard/admin/users/create.vue
+++ pages/dashboard/admin/users/create.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <DashboardAdminPageHeader title="Nouvel utilisateur" />
+        <DashboardAdminPageContent>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit dicta corporis, enim maiores totam harum numquam, quibusdam quis voluptatibus optio, laboriosam sunt. Ratione voluptatum consectetur, cupiditate beatae impedit nobis inventore? Dolorem veniam voluptates repellat iure quisquam aut facere vero exercitationem minus eaque? Quasi, harum, earum, possimus praesentium sequi cumque sit sint tempora deleniti quae repellendus ipsa cum placeat. A fugit labore quasi neque repellendus laborum saepe alias non amet libero ad quae sunt commodi quo tempore earum, sapiente eum, veniam beatae error animi eveniet porro? Sequi, veniam cupiditate nobis commodi perspiciatis placeat hic, expedita animi nam officia aliquid incidunt modi.</p>
+        </DashboardAdminPageContent>
     </div>
 </template>
 
