da26641cb47fe3effc560b4deb174660309d29d5
Lala <lala@mg-consulting-it-act.com>
2025-04-13 05:23:19 +0300

    chore: refactor

--- components/ui/table/TableRow.vue
+++ components/ui/table/TableRow.vue
@@ -2,13 +2,19 @@
 import type { HTMLAttributes } from 'vue';
 import { cn } from '@/lib/utils';
 
+const { isAdmin } = useAuth();
 const props = defineProps<{
     class?: HTMLAttributes['class'];
 }>();
 </script>
 
 <template>
-    <tr :class="cn('border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted', props.class)">
+    <tr
+        :class="cn('border-b transition-colors', {
+            'hover:bg-gray-100 data-[state=selected]:bg-gray-100': isAdmin,
+            'hover:bg-muted/50 data-[state=selected]:bg-bg-muted/50': !isAdmin,
+        }, props.class)"
+    >
         <slot />
     </tr>
 </template>
