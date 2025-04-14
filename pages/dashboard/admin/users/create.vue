b2d27f0c282b75636551c0e10963fd9a0f129b88
Lala <lala@mg-consulting-it-act.com>
2025-04-14 11:56:11 +0300

    chore: prepare create user form in admin

--- pages/dashboard/admin/users/create.vue
+++ pages/dashboard/admin/users/create.vue
@@ -1,8 +1,113 @@
 <template>
     <div>
         <DashboardAdminPageHeader title="Nouvel utilisateur" />
-        <DashboardAdminPageContent>
-           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit dicta corporis, enim maiores totam harum numquam, quibusdam quis voluptatibus optio, laboriosam sunt. Ratione voluptatum consectetur, cupiditate beatae impedit nobis inventore? Dolorem veniam voluptates repellat iure quisquam aut facere vero exercitationem minus eaque? Quasi, harum, earum, possimus praesentium sequi cumque sit sint tempora deleniti quae repellendus ipsa cum placeat. A fugit labore quasi neque repellendus laborum saepe alias non amet libero ad quae sunt commodi quo tempore earum, sapiente eum, veniam beatae error animi eveniet porro? Sequi, veniam cupiditate nobis commodi perspiciatis placeat hic, expedita animi nam officia aliquid incidunt modi.</p>
+        <DashboardAdminPageContent class="bg-gray-100">
+            <form>
+                <div class="grid grid-cols-[40%_60%] gap-4 lg:gap-8">
+                    <div class="p-4">
+                        <h1 class="font-semibold text-gray-600">
+                            Information
+                        </h1>
+                        <p class="mt-2 text-md text-gray-500">
+                            Les information personnelles
+                        </p>
+                    </div>
+                    <div class="bg-white p-4 rounded-md flex flex-col gap-4">
+                        <div>
+                            <InputIcon
+                                rounded="md"
+                                label="Nom"
+                                name="lastname"
+                            />
+                        </div>
+                        <div>
+                            <InputIcon
+                                rounded="md"
+                                label="Prénom"
+                                name="firstname"
+                            />
+                        </div>
+                        <div>
+                            <InputIcon
+                                rounded="md"
+                                label="INAMI"
+                                name="identifier_number"
+                            />
+                        </div>
+                        <div>
+                            <InputIcon
+                                rounded="md"
+                                type="email"
+                                label="Email"
+                                name="email"
+                            />
+                        </div>
+                        <div>sexe</div>
+                        <div>
+                            <InputIcon
+                                rounded="md"
+                                label="Téléphone"
+                                name="phone_number"
+                            />
+                        </div>
+                        <div>
+                            <InputIcon
+                                rounded="md"
+                                label="Date de naissance"
+                                name="date_of_birth"
+                            />
+                        </div>
+                        <div>
+                            <InputIcon
+                                rounded="md"
+                                label="Langue"
+                                name="language"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <Separator class="my-10" />
+                <div class="grid grid-cols-[40%_60%] gap-4 lg:gap-8">
+                    <div class="p-4">
+                        <h1 class="font-semibold text-gray-600">
+                            Adresse
+                        </h1>
+                        <p class="mt-2 text-md text-gray-500">
+                            Les informations d'addresse
+                        </p>
+                    </div>
+                    <div class="bg-white p-4 rounded-md flex flex-col gap-4">
+                        <div>
+                            <InputIcon
+                                rounded="md"
+                                label="N° et rue"
+                                name="street"
+                            />
+                        </div>
+                        <div>
+                            <InputIcon
+                                rounded="md"
+                                label="Ville"
+                                name="city"
+                            />
+                        </div>
+                        <div>
+                            <InputIcon
+                                rounded="md"
+                                label="Code postale"
+                                name="zipCode"
+                            />
+                        </div>
+                        <div>
+                            <InputIcon
+                                rounded="md"
+                                label="Complément d'adresse"
+                                name="additionnalInformation"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </form>
         </DashboardAdminPageContent>
     </div>
 </template>
