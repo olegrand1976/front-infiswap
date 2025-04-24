353797354d22ecc6bcbfa5bbed335ae7b9b6239f
jerry <jerry@mg-consulting-it-act.com>
2025-04-24 16:09:20 +0300

    chore: replace text in subscription plan for dashboard

--- pages/dashboard/subscriptions/index.vue
+++ pages/dashboard/subscriptions/index.vue
@@ -24,7 +24,22 @@
                     <div class="mt-8 space-y-8 md:-mx-4 md:flex md:items-center md:justify-center md:space-y-0 xl:mt-0">
                         <div class="max-w-sm mx-auto h-full border rounded-lg md:mx-4 dark:border-gray-700">
                             <div class="p-6 text-center">
-                                <h1
+                                <h1 class="text-success text-3xl font-semibold">
+                                    <span>100% gratuit</span>
+                                </h1>
+
+                                <p class="mt-4 max-w-sm text-gray-500 dark:text-gray-300">
+                                    InfiSwap est accessible et utilisable 100% gratuitement. Profitez en pour utiliser et tester l'ensemble de nos services qui vont évoluer ces prochains mois.
+                                    N'hésitez pas à <NuxtLink
+                                        to="/contact"
+                                        class="text-primary"
+                                    >nous contacter</NuxtLink> sur : <a
+                                        href="mailto:info@infiswap.be"
+                                        class="text-primary font-bold"
+                                        target="_blank"
+                                    >info@infiswap.be</a> ou au 0478.02.33.77 pour plus d'informations.
+                                </p>
+                                <!-- <h1
                                     v-if="current?.plan?.description"
                                     class="text-xl font-medium text-success capitalize lg:text-2xl dark:text-white"
                                 >
@@ -33,7 +48,7 @@
 
                                 <p class="mt-4 max-w-sm text-gray-500 dark:text-gray-300">
                                     Nous vous remercions pour votre confiance et votre abonnement à notre service. Vous pouvez profiter dès maintenant de toutes les fonctionnalités que nous proposons.
-                                </p>
+                                </p> -->
 
                                 <!-- <span>Vous êtes désormais abonnée à [Nom du service]</span> -->
                                 <!-- <h2 class="mt-4 text-2xl font-semibold text-gray-700 sm:text-3xl dark:text-gray-300">
