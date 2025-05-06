7582be19940eb0d9d33bd8eb7f25b30b31423847
Lala <lala@mg-consulting-it-act.com>
2025-05-06 07:28:39 +0300

    chore: refactor filter by name and zip code

--- pages/dashboard/admin/users/index.vue
+++ pages/dashboard/admin/users/index.vue
@@ -6,22 +6,26 @@
             </template>
         </DashboardAdminPageHeader>
         <DashboardAdminPageContent>
-            <div class="p-4 flex gap-3">
+            <div class="p-4 flex gap-3 items-center">
                 <InputIcon
-                    v-model="name"
+                    v-model="option.name"
                     rounded="md"
                     placeholder="Filter par Nom ou Prénom"
                     class="max-w-sm"
-                    @input="filterUsers('name')"
+                    @input="filterUsers()"
                 />
                 <InputIcon
-                    v-model="zipCode"
+                    v-model="option.zip"
                     rounded="md"
                     placeholder="Filter par C.P"
                     class="max-w-sm"
                     type="number"
-                    @input="filterUsers('zip')"
+                    @input="filterUsers()"
                 />
+                <Button class="rounded-md" @click="resetFilter">
+                    <ArrowPathIcon class="md:mr-2" />
+                    <span class="hidden md:inline-block">Restaurer</span>
+                </Button>
             </div>
             <DataTable
                 :data="dataUsers"
@@ -43,7 +47,7 @@
 <script setup lang="ts">
 import { h } from 'vue';
 import type { ColumnDef } from '@tanstack/vue-table';
-import { XCircleIcon, ArrowsUpDownIcon } from '@heroicons/vue/24/solid';
+import { XCircleIcon, ArrowsUpDownIcon, ArrowPathIcon } from '@heroicons/vue/24/solid';
 import { Button } from '@/components/ui/button';
 import { formatInamiNumber, formatPhoneNumber } from '~/lib/utils';
 import type { User } from '~/lib/types';
@@ -60,12 +64,13 @@ definePageMeta({
 });
 const { users, getUsers, forceDelete, resendEmailVerification, validate } = useAuth();
 
-const zipCode = ref(null);
-const name = ref('');
-
 const perPage = ref(PERPAGE);
 const page = ref(1);
-const option = ref(null);
+const initialFilter = {
+    name: null,
+    zip: null,
+};
+const option = ref({ ...initialFilter });
 
 await getUsers(page.value, perPage.value, option.value);
 
@@ -80,10 +85,19 @@ const handlePerPageChange = async (value: number) => {
     await getUsers(page.value, value, option.value);
 };
 
-const filterUsers = async (by: 'zip' | 'name') => {
-    option.value = by === 'zip' ? { zip: zipCode.value } : { name: name.value };
+const filterUsers = async () => {
+    await getUsers(page.value, perPage.value, { ...option.value });
+};
 
-    await getUsers(page.value, perPage.value, option.value);
+const resetFilter = async () => {
+    const isSame = JSON.stringify(option.value) === JSON.stringify(initialFilter);
+    if (isSame) {
+        return;
+    }
+
+    option.value = { ...initialFilter };
+
+    await getUsers();
 };
 
 const columns: ColumnDef<User>[] = [
