d564fc4aed7c0843d0d149dae7dc06c91077e18b
mialivola <mialivolarandrianaivo@gmail.com>
2025-06-13 09:44:34 +0300

    create front nursetech

--- components/nurse/Banner.vue
+++ components/nurse/Banner.vue
@@ -17,14 +17,14 @@
         <div class="absolute -right-10 top-[4rem] h-[700px] w-[1200px] z-20">
             <LayoutsAppImage
                 src="white_triangle.png"
-                class="w-full h-full object-contain"
+                class="w-full h-full"
             />
 
-            <div class="bottom-[6rem] absolute inset-0 flex items-center justify-center px-8 z-30 space-x-6 ml-[30rem]">
-                <div>
-                    <span class="text-5xl font-bold text-primary">Pour</span>
-                    <span>infirmière</span>
-                    <span>Infiswap</span>
+            <div class="absolute inset-0 flex items-center justify-center gap-6 px-8 pb-[5rem] z-30 ml-[22rem]">
+                <div class="flex flex-col text-right mt-[14rem]">
+                    <span class="text-7xl font-medium text-primary leading-[4.5rem]">Pour</span>
+                    <span class="text-4xl text-primary">infirmière</span>
+                    <span class="text-7xl font-medium text-primary leading-[4.5rem]">Infiswap</span>
                 </div>
 
                 <LayoutsAppImage
