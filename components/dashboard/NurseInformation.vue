0468fad5c49608d03c2c89380edabc4a51821735
Anja <anjamickenzy@gmail.com>
2025-09-18 15:14:50 +0300

    modification de la largeur et la hauteur

--- components/dashboard/NurseInformation.vue
+++ components/dashboard/NurseInformation.vue
@@ -225,8 +225,8 @@
             </div>
         </section>
 
-        <section class="flex flex-col lg:flex-row w-full">
-            <div class="w-full">
+        <section class="flex flex-col lg:flex-row w-full items-stretch">
+            <div class="w-full lg:w-1/2 bg-white rounded-lg shadow-lg">
                 <div class="bg-gray-100 rounded p-4">
                     <div class="text-primary flex justify-between items-center">
                         <h2>
@@ -288,7 +288,7 @@
                     </div>
                 </div>
 
-                <div class="bg-gray-100 flex gap-6 items-center rounded p-4">
+                <div class="w-full lg:w-1/2 bg-white rounded-lg shadow-lg flex flex-col">
                     <div class="bg-gray-200 p-4 rounded">
                         <UserIcon class="w-8 opacity-80" />
                     </div>
@@ -303,7 +303,7 @@
                 </div>
             </div>
 
-            <div class="w-full">
+            <div class="w-full lg:w-1/2 bg-white rounded-lg shadow-lg">
                 <h3 class="text-white bg-primary p-3 rounded-t-lg">
                     Nombres d'annonces auxquelles j'ai répondu sur InfiSwap par mois
                 </h3>
