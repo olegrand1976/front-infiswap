4a2dbbf96513ece959f68319e7b7362fb50738af
Lala <lala@mg-consulting-it-act.com>
2026-02-20 16:48:01 +0300

    refactor

--- app/components/home/AppDownloadSection.vue
+++ app/components/home/AppDownloadSection.vue
@@ -12,15 +12,17 @@
             "></div>
         </div>
 
-        <div class="container relative z-10 flex-col-reverse max-w-4xl lg:flex-row flex items-center justify-between">
-            <div class="relative z-10">
-                <LayoutsAppImage
-                    src="Group_1048.png"
-                    alt="App Download"
-                    class="sm:w-80 w-72 mt-24 sm:mt-0 mb-8 sm:mb-16"
-                />
+        <div class="container relative z-10 flex-col-reverse max-w-5xl lg:flex-row flex items-center justify-between">
+            <div class="relative z-10 w-full">
+                <div class="relative inline-block">
+                    <LayoutsAppImage
+                        src="Group_1048.png"
+                        alt="App Download"
+                        class="sm:w-80 w-72 mt-24 sm:mt-0 mb-8 sm:mb-16"
+                    />
+                </div>
             </div>
-            <div class="flex flex-col items-center relative z-10">
+            <div class="flex flex-col items-center relative z-10 w-full h-full">
                 <div class="sm:-mt-28 flex flex-col items-center mx-auto">
                     <h2 class="lg:text-4xl text-3xl text-center sm:text-start font-bold text-primary">
                         Télécharger
@@ -30,6 +32,12 @@
                     </p>
                 </div>
                 <div class="bg-white p-8 shadow-md border border-slate-100 rounded-lg mt-8 space-y-7 relative z-10">
+                    <div class="absolute -left-4 lg:-left-18 top-1/2 -translate-y-1/2 -translate-x-full lg:-top-40 z-20">
+                        <LayoutsAppImage
+                            src="/icons/Unin.png"
+                            class="size-16 lg:size-26"
+                        />
+                    </div>
                     <div class="relative">
                         <div class="absolute flex items-center gap-2 left-1/2 -translate-x-1/2 bottom-full mb-2 w-max px-3 py-2 text-white bg-primary rounded-lg animate-bounce-fade duration-&lsqb;5s&rsqb; z-20">
                             <ClockIcon class="size-5" /> <span>Bientôt disponible</span>
@@ -63,13 +71,6 @@
                     </Button>
                 </div>
             </div>
-        </div>
-
-        <div class="absolute top-20 sm:top-24 sm:left-36 xl:top-16 left-6 xl:left-160 lg:left-132 z-20">
-            <LayoutsAppImage
-                src="/icons/Unin.png"
-                class="xl:w-32 xl:h-32 lg:w-24 lg:h-full sm:w-16 sm:h-16 w-10 h-10"
-            />
         </div>
     </div>
 </template>
