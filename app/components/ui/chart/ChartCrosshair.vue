3e5e2a18fcb35033bfcf768910343e8263c744a7
Lala <lala@mg-consulting-it-act.com>
2026-02-24 16:14:24 +0300

    update chart

--- app/components/ui/chart/ChartCrosshair.vue
+++ app/components/ui/chart/ChartCrosshair.vue
@@ -1,71 +1,45 @@
 <script setup lang="ts">
-import type { BulletLegendItemInterface } from '@unovis/ts';
-import { omit } from '@unovis/ts';
-import { VisCrosshair, VisTooltip } from '@unovis/vue';
-import { type Component, createVNode, render } from 'vue';
-import { ChartTooltip } from '.';
+import type { BulletLegendItemInterface } from "@unovis/ts"
+import type { Component } from "vue"
+import { omit } from "@unovis/ts"
+import { VisCrosshair, VisTooltip } from "@unovis/vue"
+import { createApp } from "vue"
+import { ChartTooltip } from "."
 
 const props = withDefaults(defineProps<{
-    colors: string[];
-    index: string;
-    items: BulletLegendItemInterface[];
-    customTooltip?: Component;
-    legendLabels?: string[];
-    categories?: string[];
+  colors: string[]
+  index: string
+  items: BulletLegendItemInterface[]
+  customTooltip?: Component
 }>(), {
-    colors: () => [],
-    legendLabels: () => [],
-    categories: () => [],
-});
+  colors: () => [],
+})
 
-const wm = new WeakMap();
-
+// Use weakmap to store reference to each datapoint for Tooltip
+const wm = new WeakMap()
 function template(d: any) {
-    if (wm.has(d)) {
-        return wm.get(d);
-    }
-    else {
-        const omittedData = Object.entries(omit(d, [props.index])).map(([key, value], idx) => {
-            const label = props.legendLabels?.[idx] || props.items.find(i => i.name === key)?.name || key;
-            const legendReference = props.items.find(i => i.name === (props.legendLabels?.[idx] || key)) || {
-                name: label,
-                color: props.colors[idx] || 'transparent',
-            };
-            return { ...legendReference, value };
-        });
-
-        const container = document.createElement('div');
-
-        const vnode = createVNode(
-            props.customTooltip ?? ChartTooltip,
-            {
-                title: d[props.index]?.toString() || 'Unknown',
-                data: omittedData,
-            },
-        );
-
-        render(vnode, container);
-
-        wm.set(d, container);
-
-        return container;
-    }
+  if (wm.has(d)) {
+    return wm.get(d)
+  }
+  else {
+    const componentDiv = document.createElement("div")
+    const omittedData = Object.entries(omit(d, [props.index])).map(([key, value]) => {
+      const legendReference = props.items.find(i => i.name === key)
+      return { ...legendReference, value }
+    })
+    const TooltipComponent = props.customTooltip ?? ChartTooltip
+    createApp(TooltipComponent, { title: d[props.index].toString(), data: omittedData }).mount(componentDiv)
+    wm.set(d, componentDiv.innerHTML)
+    return componentDiv.innerHTML
+  }
 }
 
-function color(_: unknown, i: number) {
-    return props.colors[i] ?? 'transparent';
+function color(d: unknown, i: number) {
+  return props.colors[i] ?? "transparent"
 }
 </script>
 
 <template>
-    <VisTooltip
-        :horizontal-shift="30"
-        :vertical-shift="30"
-        :colors="colors"
-    />
-    <VisCrosshair
-        :template="template"
-        :color="color"
-        :colors="colors"
-    />
+  <VisTooltip :horizontal-shift="20" :vertical-shift="20" />
+  <VisCrosshair :template="template" :color="color" />
 </template>
--- app/components/ui/chart/index.ts
+++ app/components/ui/chart/index.ts
@@ -1,18 +1,40 @@
-export { default as ChartCrosshair } from './ChartCrosshair.vue';
-export { default as ChartLegend } from './ChartLegend.vue';
-export { default as ChartSingleTooltip } from './ChartSingleTooltip.vue';
-export { default as ChartTooltip } from './ChartTooltip.vue';
+import type { Component, Ref } from "vue"
+import { createContext } from "reka-ui"
 
-export function defaultColors(count: number = 3) {
-    const quotient = Math.floor(count / 2);
-    const remainder = count % 2;
+export { default as ChartContainer } from "./ChartContainer.vue"
+export { default as ChartLegend } from "./ChartLegend.vue"
+export { default as ChartLegendContent } from "./ChartLegendContent.vue"
+export { default as ChartTooltipContent } from "./ChartTooltipContent.vue"
+export { componentToString } from "./utils"
 
-    const primaryCount = quotient + remainder;
-    const secondaryCount = quotient;
-    return [
-        ...Array.from(new Array(primaryCount).keys()).map(i => `hsl(var(--vis-primary-color) / ${1 - (1 / primaryCount) * i})`),
-        ...Array.from(new Array(secondaryCount).keys()).map(i => `hsl(var(--vis-secondary-color) / ${1 - (1 / secondaryCount) * i})`),
-    ];
+export function defaultColors(count: number = 3): string[] {
+  const colors = [
+    "var(--chart-1)",
+    "var(--chart-2)",
+    "var(--chart-3)",
+    "var(--chart-4)",
+    "var(--chart-5)",
+  ]
+  return colors.slice(0, count)
 }
 
-export * from './interface';
+export const THEMES = { light: "", dark: ".dark" } as const
+
+export type ChartConfig = {
+  [k in string]: {
+    label?: string | Component
+    icon?: string | Component
+  } & (
+    | { color?: string, theme?: never }
+    | { color?: never, theme: Record<keyof typeof THEMES, string> }
+  )
+}
+
+interface ChartContextProps {
+  id: string
+  config: Ref<ChartConfig>
+}
+
+export const [useChart, provideChartContext] = createContext<ChartContextProps>("Chart")
+
+export { VisCrosshair as ChartCrosshair, VisTooltip as ChartTooltip } from "@unovis/vue"
