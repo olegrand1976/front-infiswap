b34a5e95470ce4eb1105c77c4b97f7a6f9dd8fe4
mialivola <mialivolarandrianaivo@gmail.com>
2026-03-06 11:34:31 +0300

    integration of the new homepage

---
+++ app/components/home/JoinBanner.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="relative w-full bg-white flex flex-col lg:flex-row items-center justify-center px-6 lg:px-16 pt-10 gap-8">
+        <div class="hidden lg:flex w-full lg:w-1/2 justify-center">
+            <LayoutsAppImage
+                src="/home/two_nurses_tablet.png"
+                alt="Infirmiers avec tablette"
+                class="w-full max-w-xl h-auto block"
+            />
+        </div>
+
+        <div class="w-full lg:w-1/2 flex flex-col items-start justify-center">
+            <h2 class="text-3xl lg:text-5xl font-light text-primary leading-snug mb-8">
+                Rejoignez la plateforme<br>et simplifiez la gestion<br>des remplacements<br>infirmiers.
+            </h2>
+            <Button
+                class="bg-primary text-white text-sm font-medium rounded-full px-12 py-5 hover:bg-primary/80 hover:scale-105 transition-transform duration-300"
+            >
+                En savoir plus
+            </Button>
+        </div>
+    </div>
+</template>
