6e89d9d7f844295cc4e12d3e37cb30521c3ac442
Lala <lala@mg-consulting-it-act.com>
2026-03-07 22:56:18 +0300

    run lint fix

--- app/middleware/institution.ts
+++ app/middleware/institution.ts
@@ -3,13 +3,13 @@ import { useAuth, useUser } from '~/composables/useAuth';
 
 export default defineNuxtRouteMiddleware((to) => {
     const user = useUser();
-    
+
     if (!user.value) return navigateTo('/login', { replace: true });
-    
+
     if (!user.value.email_verified_at) {
         return navigateTo('/auth/verify-email', { replace: true });
     }
-    
+
     const { isInstitution } = useAuth();
 
     if (isInstitution.value && to.path === '/dashboard') {
