f652c6a13ff606c178a09eca96769905aee57745
Lala <lala@mg-consulting-it-act.com>
2026-07-17 11:41:20 +0300

    remove unecessary line - profile_screen

--- mobile/lib/features/profile/presentation/profile_screen.dart
+++ mobile/lib/features/profile/presentation/profile_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'dart:math' as math;
 
 import '../../../core/theme/app_colors.dart';
 import '../../../core/theme/theme_mode_provider.dart';
