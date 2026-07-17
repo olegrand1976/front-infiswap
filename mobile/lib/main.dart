3f9fe78be4d1a6049b2da54b50e4b95b98917456
Lala <lala@mg-consulting-it-act.com>
2026-07-17 15:23:23 +0300

    mobile: add push module

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app.dart';
+import 'core/push/push_notification_service.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+
   runApp(
     const ProviderScope(
       child: InfiSwapApp(),
